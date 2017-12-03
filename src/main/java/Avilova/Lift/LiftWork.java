e18a4d327ee3eaf250a158173b839f60751ae642
AnastasiaAvilova <an_avilova@mail.ru>
2017-12-04 01:18:50 +0300

    Second commit whith javadoc

--- src/main/java/Avilova/Lift/LiftWork.java
+++ src/main/java/Avilova/Lift/LiftWork.java
@@ -6,6 +6,7 @@ import java.io.Console;
 public class LiftWork {
     public static void main(String[] args) {
         Console console = System.console();
+        //проверяем что консоль не null
         if(console!=null) {
             UserInterfase userInterfase = new UserInterfase(console);
             userInterfase.write();
